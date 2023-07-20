9d7093915f129214b6ea0cbadc38de8a2645ccfb
daimessdn@gmail.com <daimessdn@gmail.com>
2023-07-20 17:33:39 +0700

    added plotting feature

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from performance_curve.pipeline import Pipeline
 import performance_curve.calculations.conversion as cvt
 
+import matplotlib.pyplot as plt
+
 pl = Pipeline({
     "q_oil": 2_000,
     "mu_oil": 2,
@@ -31,3 +33,33 @@ print(pl.friction_factor_no_slip())
 print(pl.friction_factor_two_phase())
 
 print("Pressure gradient (friction):", dp_dz, "psi/ft")
+
+# Using pressure gradient, we can calculate
+## the pressure for each segment
+segment = 100 # per 100 ft
+
+depth_data = [i * segment for i in range(0, 21)]
+pressure_data = []
+
+for i in range(len(depth_data)):
+    if (i == 0):
+        pressure_data.append(pl.pressure)
+    
+    else:
+        pressure_data.append(
+            pressure_data[i -1] + (dp_dz) * segment
+        )
+
+plt.plot(pressure_data, depth_data, label="Pressure gradient")
+
+plt.ylabel("Depth (ft)")
+plt.text((pressure_data[len(pressure_data) // 3]), -250, 'Pressure (psia)')
+
+plt.subplots_adjust(top=0.85)
+
+plt.ylim(depth_data[-1] + 300, 0)
+plt.tick_params(axis='both', which='major',
+               labelsize=10, labelbottom=False,
+               bottom=False, top=True, labeltop=True)
+
+plt.show()
